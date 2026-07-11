f7cc8be47b7dee2a8d9ef25a02fbf041e1dbfef1
Feng Lengshun <bayazid.bustami.halim@gmail.com>
2026-07-11 11:23:45 +0700

    update config files

--- Documents/Containers/Arch/arch-distrobox-packages.sh
+++ Documents/Containers/Arch/arch-distrobox-packages.sh
@@ -6,11 +6,12 @@ paru -Su --noconfirm --needed --skipreview octopi aria2 rsync bat grc highlight 
 paru -Su --noconfirm --needed --skipreview firefox google-chrome microsoft-edge-stable-bin vivaldi vivaldi-ffmpeg-codecs
 paru -Su --noconfirm --needed --skipreview kate konsole kdeconnect kontrast arianna
 paru -Su --noconfirm --needed --skipreview bottles faugus-launcher cachyos-gaming-meta
-paru -Su --noconfirm --needed --skipreview fsearch-git sqlitebrowser github-desktop grsync android-tools git-delta grex czkawka-gui
-paru -Su --noconfirm --needed --skipreview gallery-dl media-downloader peazip qdirstat vlc mcomix qbittorrent masterpdfeditor-free
+paru -Su --noconfirm --needed --skipreview grsync android-tools git-delta grex czkawka-gui
+paru -Su --noconfirm --needed --skipreview gallery-dl media-downloader masterpdfeditor-free
 
 # AUR specific apps
 paru -Su --noconfirm --needed --skipreview whitesur-gtk-theme whitesur-icon-theme whitesur-kde-theme
 paru -Su --noconfirm --needed --skipreview proton-authenticator-bin proton-pass-bin
 paru -Su --noconfirm --needed --skipreview mangal-bin mov-cli mangayomi-linux adbtuifm
 paru -Su --noconfirm --needed --skipreview grayjay-bin edconv-bin soundkonverter-qt6-git
+paru -Su --noconfirm --needed --skipreview onthespot-appimage soundbound-app-bin spotiflyer-bin
